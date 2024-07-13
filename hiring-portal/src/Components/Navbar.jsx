5a96a9b5fadf493e4619d06d8f37cf2ecd918ae7
Yashgabani845 <gabaniyash845@gmail.com>
2024-07-13 12:37:55 +0530

    navbar basic added

--- hiring-portal/src/Components/Navbar.jsx
+++ hiring-portal/src/Components/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "../CSS/navbar.css"
 const Navbar = ()=>{
     return(
-        <div className="navbar"></div>
+        <div className="navbar">
+        <div className="logo">Hirehub</div>
+        <div className="searchdiv"><input type="text" className="search" placeholder="  Search jobs" name="" id="" /></div>
+        <div className="icons">
+            <div className="home">Home</div>
+            <div className="jobs">Jobs</div>
+            <div className="aboutus">About</div>
+            <div className="login">Login</div>
+        </div>
+        </div>
     )
 }
 export default Navbar;
