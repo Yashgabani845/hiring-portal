807c0e2f499b950549ed5eeacd4ed20ebee138b3
Yashgabani845 <gabaniyash845@gmail.com>
2024-07-13 16:36:33 +0530

    navbar done

--- hiring-portal/src/Components/Navbar.jsx
+++ hiring-portal/src/Components/Navbar.jsx
@@ -1,17 +1,45 @@
 import React from "react";
-import "../CSS/navbar.css"
-const Navbar = ()=>{
-    return(
-        <div className="navbar">
-        <div className="logo">Hirehub</div>
-        <div className="searchdiv"><input type="text" className="search" placeholder="  Search jobs" name="" id="" /></div>
-        <div className="icons">
-            <div className="home">Home</div>
-            <div className="jobs">Jobs</div>
-            <div className="aboutus">About</div>
-            <div className="login">Login</div>
+import "../CSS/navbar.css";
+import HomeIcon from "@mui/icons-material/Home";
+import WorkIcon from "@mui/icons-material/Work";
+import InfoIcon from "@mui/icons-material/Info";
+import LoginIcon from "@mui/icons-material/Login";
+import SearchIcon from "@mui/icons-material/Search";
+import logo from "../logo.png"
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo"><img className="logoimg" src={logo} alt="" /></div>
+      <div className="searchdiv">
+        <input
+          type="text"
+          className="search"
+          placeholder="Search jobs"
+          name=""
+          id=""
+        />
+        <SearchIcon className="search-icon" />
+      </div>
+      <div className="icons">
+        <div className="icon home">
+          <HomeIcon />
+          <span>Home</span>
         </div>
+        <div className="icon jobs">
+          <WorkIcon />
+          <span>Jobs</span>
         </div>
-    )
-}
+        <div className="icon aboutus">
+          <InfoIcon />
+          <span>About</span>
+        </div>
+        <div className="icon login">
+          <LoginIcon />
+          <span>Login</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
