c0c41e7ff22a565749621f9816172707e66ccd41
Sawan <sawankushwah457@gmail.com>
2024-10-04 17:42:00 +0530

    Make home page reponsive SK

--- hiring-portal/src/Components/MiddleView.jsx
+++ hiring-portal/src/Components/MiddleView.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../CSS/middleview.css";
 import jobimg from "../jobs.png";
 import LazyLoad from 'react-lazyload';
-
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 const roles = [
     { role: "Software Developer", jobs: 38 },
     { role: "Data Scientist", jobs: 24 },
@@ -17,19 +17,36 @@ const roles = [
 ];
 
 const MiddleView = () => {
+    const rolesListRef = useRef(null);
+
+    const scrollNext = () => {
+        const roleWidth = rolesListRef.current.querySelector('.role').offsetWidth;
+        rolesListRef.current.scrollBy({ left: roleWidth + 10, behavior: 'smooth' });
+    };
+
+    const scrollPrev = () => {
+        const roleWidth = rolesListRef.current.querySelector('.role').offsetWidth;
+        rolesListRef.current.scrollBy({ left: -(roleWidth + 10), behavior: 'smooth' });
+    };
     return (
         <div className="middleview">
             <div className="text-part">
                 <h1>Discover Jobs Across Popular Roles</h1>
-            <center>   <p>Select a role and we'll show you relevant jobs for it!</p></center> 
-                <div className="roles-carousel">
-                    <div className="roles-list">
+                <center>   <p>Select a role and we'll show you relevant jobs for it!</p></center>
+                <div className="roles-carousel" style={{ padding: '0px 2rem', position: 'relative' }}>
+                    <button className="scroll-btn prev-btn" onClick={scrollPrev} >
+                        <FaChevronLeft size={24} />
+                    </button>
+                    <div className="roles-list" ref={rolesListRef} >
                         {roles.map((item, index) => (
-                            <div className="role" key={index}>
+                            <div className="role" key={index} >
                                 <span>{item.role}</span> <span className="job-count">jobs</span>
                             </div>
                         ))}
                     </div>
+                    <button className="scroll-btn next-btn" onClick={scrollNext}>
+                        <FaChevronRight size={24} />
+                    </button>
                 </div>
             </div>
             <div className="jobimg">
