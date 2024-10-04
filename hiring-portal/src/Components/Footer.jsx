5a4abfb1712e6eb8b63166fb9afc92801aec035d
Gabani yash <127788321+Yashgabani845@users.noreply.github.com>
2024-10-04 18:39:31 +0530

    Revert "Make home page reponsive "

--- hiring-portal/src/Components/Footer.jsx
+++ hiring-portal/src/Components/Footer.jsx
@@ -7,43 +7,37 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
-                <div className="flex">
-
-                    <div className="footer-column">
-                        <h3>Company</h3>
-                        <ul>
-                            <li><a href="/about">About Us</a></li>
-                            <li><a href="/">Careers</a></li>
-                            <li><a href="/">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div className="footer-column">
-                        <h3>Resources</h3>
-                        <ul>
-                            <li><a href="/">Blog</a></li>
-                            <li><a href="/">FAQs</a></li>
-                            <li><a href="/">Support</a></li>
-                        </ul>
-                    </div>
+                <div className="footer-column">
+                    <h3>Company</h3>
+                    <ul>
+                        <li><a href="/about">About Us</a></li>
+                        <li><a href="/">Careers</a></li>
+                        <li><a href="/">Contact Us</a></li>
+                    </ul>
                 </div>
-                <div className="flex">
-
-
-                    <div className="footer-column">
-                        <h3>Legal</h3>
-                        <ul>
-                            <li><a href="/">Privacy Policy</a></li>
-                            <li><a href="/">Terms of Service</a></li>
-                        </ul>
-                    </div>
-                    <div className="footer-column">
-                        <h3>Follow Us</h3>
-                        <div className="social-icons">
-                            <a href="https://www.linkedin.com/in/yash-gabani-527886258/"><FaLinkedin /></a>
-                            <a href="https://x.com/Hirehub280355"><FaSquareXTwitter /></a>
-                            <a href="https://github.com/Yashgabani845"><FaGithub /></a>
-                            <a href="https://www.instagram.com/yash845_/"><FaFacebook /></a>
-                            <a href="https://www.instagram.com/yash845_/"><FaInstagram /></a>
+                <div className="footer-column">
+                    <h3>Resources</h3>
+                    <ul>
+                        <li><a href="/">Blog</a></li>
+                        <li><a href="/">FAQs</a></li>
+                        <li><a href="/">Support</a></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h3>Legal</h3>
+                    <ul>
+                        <li><a href="/">Privacy Policy</a></li>
+                        <li><a href="/">Terms of Service</a></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h3>Follow Us</h3>
+                    <div className="social-icons">
+                        <a  href="https://www.linkedin.com/in/yash-gabani-527886258/" class="linkedin"><FaLinkedin /></a>
+                        <a href="https://x.com/Hirehub280355" class="twitter"><FaTwitter /></a>
+                        <a href="https://github.com/Yashgabani845" class="github"><FaGithub/></a>
+                        <a href="https://www.instagram.com/yash845_/" class="facebook"><FaFacebook /></a>
+                        <a href="https://www.instagram.com/yash845_/" class="instagram"><FaInstagram /></a>
                     </div>
                 </div>
             </div>
