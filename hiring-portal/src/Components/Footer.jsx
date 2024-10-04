70853bf4180cf62cd309c5e6946c63c6a04e8c25
Yashgabani845 <gabaniyash845@gmail.com>
2024-10-04 18:45:18 +0530

    rever merge conflicts

--- hiring-portal/src/Components/Footer.jsx
+++ hiring-portal/src/Components/Footer.jsx
@@ -1,46 +1,48 @@
 import React from "react";
 import "../CSS/footer.css";
 import { FaLinkedin, FaTwitter, FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
-// import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
-                <div className="footer-column">
-                    <h3>Company</h3>
-                    <ul>
-                        <li><a href="/about">About Us</a></li>
-                        <li><a href="/">Careers</a></li>
-                        <li><a href="/">Contact Us</a></li>
-                    </ul>
-                </div>
-                <div className="footer-column">
-                    <h3>Resources</h3>
-                    <ul>
-                        <li><a href="/">Blog</a></li>
-                        <li><a href="/">FAQs</a></li>
-                        <li><a href="/">Support</a></li>
-                    </ul>
-                </div>
-                <div className="footer-column">
-                    <h3>Legal</h3>
-                    <ul>
-                        <li><a href="/">Privacy Policy</a></li>
-                        <li><a href="/">Terms of Service</a></li>
-                    </ul>
-                </div>
-                <div className="footer-column">
-                    <h3>Follow Us</h3>
-                    <div className="social-icons">
-                        <a  href="https://www.linkedin.com/in/yash-gabani-527886258/" class="linkedin"><FaLinkedin /></a>
-                        <a href="https://x.com/Hirehub280355" class="twitter"><FaTwitter /></a>
-                        <a href="https://github.com/Yashgabani845" class="github"><FaGithub/></a>
-                        <a href="https://www.instagram.com/yash845_/" class="facebook"><FaFacebook /></a>
-                        <a href="https://www.instagram.com/yash845_/" class="instagram"><FaInstagram /></a>
+                <div className="flex">
+                    <div className="footer-column">
+                        <h3>Company</h3>
+                        <ul>
+                            <li><a href="/about">About Us</a></li>
+                            <li><a href="/">Careers</a></li>
+                            <li><a href="/">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-column">
+                        <h3>Resources</h3>
+                        <ul>
+                            <li><a href="/">Blog</a></li>
+                            <li><a href="/">FAQs</a></li>
+                            <li><a href="/">Support</a></li>
+                        </ul>
                     </div>
                 </div>
-            </div>
+                <div className="flex">
+                    <div className="footer-column">
+                        <h3>Legal</h3>
+                        <ul>
+                            <li><a href="/">Privacy Policy</a></li>
+                            <li><a href="/">Terms of Service</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-column">
+                        <h3>Follow Us</h3>
+                        <div className="social-icons">
+                            <a href="https://www.linkedin.com/in/yash-gabani-527886258/"><FaLinkedin /></a>
+                            <a href="https://twitter.com/Hirehub280355"><FaTwitter /></a>
+                            <a href="https://github.com/Yashgabani845"><FaGithub /></a>
+                            <a href="https://www.instagram.com/yash845_/"><FaInstagram /></a>
+                            <a href="https://www.facebook.com/"><FaFacebook /></a> {/* Assuming you're using this for Facebook */}
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="footer-bottom">
                 <p>&copy; 2024 Hirehub. All rights reserved.</p>
