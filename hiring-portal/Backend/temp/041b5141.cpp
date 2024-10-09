e9ead9c38cd5e30ae4ebfa5af370f30ecfdf991e
Rafat Alam <126316989+rafat-alam@users.noreply.github.com>
2024-10-09 08:38:12 +0530

    Update 041b5141.cpp
    I have decrease the space space complexity.

--- hiring-portal/Backend/temp/041b5141.cpp
+++ hiring-portal/Backend/temp/041b5141.cpp
@@ -4,10 +4,6 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    
-    int sum = a + b;
-    
-    cout << sum << endl;
-    
+    cout << (a + b) << endl;
     return 0;
 }
