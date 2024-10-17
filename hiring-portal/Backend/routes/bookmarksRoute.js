fd68e750c87ea256e318e0294aac3645dfeb0e4c
Gabani yash <127788321+Yashgabani845@users.noreply.github.com>
2024-10-17 15:11:03 +0530

    Revert "Added bookmarks api on backend"

--- hiring-portal/Backend/routes/bookmarksRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const bookmarksController=require('../controllers/bookmarksController');
-
-router.post('/bookmarks/addBookmark',bookmarksController.addBookmark);
-router.delete('/bookmarks/removeJobFromBookmark',bookmarksController.removeJobFromBookmark);
-router.get('/bookmarks/getBookmarks',bookmarksController.getBookmarks);
